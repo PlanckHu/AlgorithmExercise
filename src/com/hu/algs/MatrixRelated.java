cf5fd147ad9031719cda4ccf6be1f33ea3a01b05
PlanckHu <planck_hu@163.com>
2019-09-17 20:09:29 +0800

    add：找极值

--- src/com/hu/algs/MatrixRelated.java
+++ src/com/hu/algs/MatrixRelated.java
@@ -73,4 +73,28 @@ public class MatrixRelated {
         }
         return res;
     }
+
+    /**
+     * 二分法查找数组中的极小值（局部最小值）
+     */
+    public int getLessIndex(int[] arr) {
+        if (arr == null || arr.length == 0)
+            return -1;
+        if (arr.length == 1 || arr[0] < arr[1])
+            return 0;
+        if (arr[arr.length - 1] < arr[arr.length - 2])
+            return arr.length - 1;
+        int left = 1;
+        int right = arr.length - 2;
+        while (left < right) {
+            int mid = (left + right) / 2;
+            if (arr[mid] > arr[mid - 1])
+                right = mid - 1;
+            else if (arr[mid] > arr[mid + 1])
+                left = mid + 1;
+            else return mid;
+        }
+        return left;
+    }
+
 }
